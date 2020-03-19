15049fbadb05fc588415d34374b944ce92130ee3
alexbaumgertner <alex.baumgertner@gmail.com>
2020-03-19 23:11:20 +0300

    Fix mongo migrate

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -1,28 +1,29 @@
-// In this file you can configure migrate-mongo
+const internalIp = require('internal-ip');
 
-const mongodbUrl = `mongodb://${process.env.APP_MONGO_USER}:${process.env.APP_MONGO_PASS}@localhost:${process.env.MONGO_PORT}`
-
-const config = {
-  mongodb: {
-    // TODO Change (or review) the url to your MongoDB:
-    url: mongodbUrl,
-
-    databaseName: process.env.APP_MONGO_DB,
-
-    options: {
-      useNewUrlParser: true, // removes a deprecation warning when connecting
-      useUnifiedTopology: true, // removes a deprecating warning when connecting
-      //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
-      //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
+const getConfig = async () => {
+  // Mongo in Docker
+  const hostIp = await internalIp.v4()
+  const mongoUrl = `mongodb://${process.env.APP_MONGO_USER}:${process.env.APP_MONGO_PASS}@${hostIp}:${process.env.MONGO_PORT}/${process.env.APP_MONGO_DB}`
+  return {
+    mongodb: {
+      url: mongoUrl,
+      databaseName: process.env.APP_MONGO_DB,
+      options: {
+        useNewUrlParser: true, // removes a deprecation warning when connecting
+        useUnifiedTopology: true, // removes a deprecating warning when connecting
+        //   connectTimeoutMS: 3600000, // increase connection timeout to 1 hour
+        //   socketTimeoutMS: 3600000, // increase socket timeout to 1 hour
+      },
     },
-  },
 
-  // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
-  migrationsDir: "migrations",
+    // The migrations dir, can be an relative or absolute path. Only edit this when really necessary.
+    migrationsDir: "migrations",
 
-  // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
-  changelogCollectionName: "changelog",
+    // The mongodb collection where the applied changes are stored. Only edit this when really necessary.
+    changelogCollectionName: "changelog",
+  }
+
 }
 
 // Return the config as a promise
-module.exports = config
+module.exports = getConfig()
