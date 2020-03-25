d31eac853ab27f2382dd35450d7a0d17338d304a
alexbaumgertner <alex.baumgertner@gmail.com>
2020-03-25 23:25:49 +0300

    Add lessons megration

---
+++ migrations/20200325202211-create_lessons.js
@@ -0,0 +1,19 @@
+module.exports = {
+  async up(db, client) {
+    await db.collection('lessons').insert([
+      {
+        name: 'Какие бывают node-js фреймворки'
+      },
+      {
+        name: 'Разбор express'
+      },
+      {
+        name: 'Разбор hapyjs'
+      }
+    ])
+  },
+
+  async down(db, client) {
+    await db.collection('lessons').deleteMany()
+  }
+};
