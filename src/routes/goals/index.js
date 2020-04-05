22501bd1402cf91af57c2a197d87775603755c53
alexbaumgertner <alex.baumgertner@gmail.com>
2020-04-05 21:33:58 +0300

    Get Goals with Skills and Lessons

--- src/routes/goals/index.js
+++ src/routes/goals/index.js
@@ -6,7 +6,16 @@ module.exports = (server) =>
     path: '/goals',
     handler: async (request, h) => {
       try {
-        const goalsAll = await GoalModel.find({}).exec()
+        const goalsAll = await GoalModel.find({})
+          .populate({
+            path: 'skills',
+            limit: 5,
+            populate: {
+              path: 'lessons',
+              limit: 5,
+            },
+          })
+          .exec()
         return h.response(goalsAll)
       } catch (error) {
         return h.response(error).code(500)
