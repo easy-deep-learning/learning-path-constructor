cf1bea50af186beb46879be39b1c8b30fd7d99ce
alexbaumgertner <alex.baumgertner@gmail.com>
2020-04-19 15:26:02 +0300

    Continue adding session login/logout

--- src/routes/login/index.js
+++ src/routes/login/index.js
@@ -1,15 +1,32 @@
 const Bell = require('@hapi/bell')
-
 const addLoginFacebook = require('./facebook')
+
+// TODO: usr internals = {}
+// @see https://gist.github.com/hueniverse/a06f6315ea736ed1b46d
 
 const registerBell = async (server) => {
   await server.register(Bell)
 }
 
-// TODO
-/*const registerSessionMongo = (server) => {
+const authSessionMongoScheme = (server, options) => ({
+  /**
+   * @see https://hapi.dev/api/?v=19.1.1#authentication-scheme
+   */
 
-}*/
+  // a lifecycle method function called for each incoming request
+  authenticate: async (request, h) => {
+    const sessionId = request.state.sessionId
+
+    /*h.authenticated(data)
+    h.unauthenticated(error, [data])*/
+  },
+  verify: async (auth) => {},
+})
+
+// TODO
+const registerSessionMongo = (server) => {
+  server.auth.scheme('session-mongo', authSessionMongoScheme)
+}
 
 module.exports = async (server) => {
   // https://hapi.dev/tutorials/cookies/
