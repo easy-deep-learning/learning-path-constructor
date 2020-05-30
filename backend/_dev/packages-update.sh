2352ca85f25bf567d52188d22f469d36b3c05a84
alexbaumgertner <alex.baumgertner@gmail.com>
2020-05-30 13:17:15 +0300

    Update packages and Mongo

---
+++ backend/_dev/packages-update.sh
@@ -0,0 +1,15 @@
+npm i \
+  @hapi/bell@latest \
+  @hapi/boom@latest \
+  @hapi/cookie@latest \
+  @hapi/hapi@latest \
+  @hapi/joi@latest \
+  hapi-pino@latest \
+  internal-ip@latest \
+  migrate-mongo@latest \
+  mongoose@latest \
+  nanoid@latest \
+  pino@latest \
+  pino-gelf@latest \
+  pino-pretty@latest \
+  snyk@latest
