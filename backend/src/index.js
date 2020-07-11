41c6345b5b942c808a8b61a5bd02d409336d0f3a
alexbaumgertner <alex.baumgertner@gmail.com>
2020-07-11 19:07:48 +0300

    Start moving to fastify: routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,35 +3,10 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
-fastify.route({
-  method: 'GET',
-  url: '/',
-  schema: {
-    // request needs to have a querystring with a `name` parameter
-    querystring: {
-      name: { type: 'string' },
-    },
-    // the response needs to be an object with an `hello` property of type 'string'
-    response: {
-      200: {
-        type: 'object',
-        properties: {
-          hello: { type: 'string' },
-        },
-      },
-    },
-  },
-  // this function is executed for every request before the handler is executed
-  preHandler: async (request, reply) => {
-    // E.g. check authentication
-  },
-  handler: async (request, reply) => {
-    return { hello: 'world' }
-  },
-})
-
 const start = async () => {
   try {
+    fastify.register(require('./routes'))
+
     await fastify.listen(process.env.APP_PORT)
     fastify.log.info(`server listening on ${fastify.server.address().port}`)
   } catch (err) {
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,79 +1,32 @@
-const Boom = require('@hapi/boom')
+// our-first-route.js
 
-const SessionModel = require('../models/SessionModel')
-
-const authSessionMongoScheme = (/*server, options*/) => {
-  return {
-    /**
-     * @see https://hapi.dev/api/?v=19.1.1#authentication-scheme
-     */
-    authenticate: async (request, h) => {
-      const sessionCookieId = request.state.sessionId
-      if (!sessionCookieId) {
-        return h.unauthenticated(Boom.unauthorized(null, 'cookie'))
-      }
-
-      const session = await SessionModel.findOne({ sessionCookieId }).exec()
-
-      if (session) {
-        const { _id, userId, isActive } = session
-
-        return h.authenticated({
-          credentials: {
-            _id,
-            userId,
-            isActive,
-          },
-        })
-      } else {
-        return h.unauthenticated(Boom.unauthorized(null, 'session'))
-      }
-    },
-  }
-}
-
-// TODO
-const registerSessionMongo = (server) => {
-  server.auth.scheme('session-mongo-scheme', authSessionMongoScheme)
-  server.auth.strategy('session-mongo', 'session-mongo-scheme', {})
-}
-
-const makeRouters = async (server) => {
-  // https://hapi.dev/tutorials/cookies/
-  // Cookie name for session
-  server.state('sessionId', {
-    isSecure: process.env.APP_PROTOCOL === 'https',
-    path: '/',
-  })
-
-  await registerSessionMongo(server)
-
-  /* Root */
-  server.route({
+async function routes(fastify, options) {
+  fastify.route({
     method: 'GET',
-    path: '/',
-    handler: (/* request, h */) => {
-      return 'Hello World!'
+    url: '/',
+    schema: {
+      // request needs to have a querystring with a `name` parameter
+      querystring: {
+        name: { type: 'string' },
+      },
+      // the response needs to be an object with an `hello` property of type 'string'
+      response: {
+        200: {
+          type: 'object',
+          properties: {
+            hello: { type: 'string' },
+          },
+        },
+      },
+    },
+    // this function is executed for every request before the handler is executed
+    preHandler: async (request, reply) => {
+      // E.g. check authentication
+    },
+    handler: async (request, reply) => {
+      return { hello: 'world 1' }
     },
   })
-
-  /* Login */
-  await require('./login')(server)
-
-  /* Logout */
-  require('./logout')(server)
-
-  /* Goals */
-  require('./goals')(server)
-
-  /* Lessons */
-  require('./lessons')(server)
-
-  /* Skills */
-  require('./skills')(server)
-
-  /* Profile */
-  require('./profile')(server)
 }
 
-module.exports = makeRouters
+module.exports = routes
