a885f05273052673e5471146aebe717a084c144a
alexbaumgertner <alex.baumgertner@gmail.com>
2021-01-12 12:01:06 +0300

    Update npm libs

---
+++ frontend/_dev/packages-update.sh
@@ -0,0 +1,11 @@
+npm i \
+  @bem-react/classname@latest \
+  @testing-library/jest-dom@latest \
+  @testing-library/react@latest \
+  @testing-library/user-event@latest \
+  prop-types@latest \
+  react@latest \
+  react-dom@latest \
+  react-router-dom@latest \
+  react-scripts@latest \
+  semantic-ui-react@latest
